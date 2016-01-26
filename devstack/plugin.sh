54fe2bc3d2046eb567da1728dae4b973bc0fe553
Akihiro Motoki <motoki@da.jp.nec.com>
2016-01-26 21:51:33 +0900

    Update devstack plugin
    * Use setup_develop to install python module as most projects do.
      This also makes translation management easier in development.
    * Deploy 'enabled' files into horizon local/enabled directory.
    * collectstatic and compress are no longer required during horizon
      plugin installation. The timing of horizon installation was fixed
      in https://review.openstack.org/#/c/257744/
    
    Change-Id: I122d94af4c5e30ffa9e6c198460647073aace722

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,11 +3,11 @@
 TROVE_DASHBOARD_DIR=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
 
 function install_trove_dashboard {
-    sudo pip install --upgrade ${TROVE_DASHBOARD_DIR}
+    setup_develop ${TROVE_DASHBOARD_DIR}
+}
 
-    cp -a ${TROVE_DASHBOARD_DIR}/trove_dashboard/enabled/* ${DEST}/horizon/openstack_dashboard/enabled/
-    python ${DEST}/horizon/manage.py collectstatic --noinput
-    python ${DEST}/horizon/manage.py compress --force
+function configure_trove_dashboard {
+    cp -a ${TROVE_DASHBOARD_DIR}/trove_dashboard/enabled/* ${DEST}/horizon/openstack_dashboard/local/enabled/
 }
 
 # check for service enabled
@@ -20,13 +20,13 @@ if is_service_enabled trove-dashboard; then
 
     elif [[ "$1" == "stack" && "$2" == "install"  ]]; then
         # Perform installation of service source
-        # no-op
-        :
+        echo_summary "Installing Trove UI"
+        install_trove_dashboard
 
     elif [[ "$1" == "stack" && "$2" == "post-config"  ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Installing Trove UI"
-        install_trove_dashboard
+        echo_summary "Configurng Trove UI"
+        configure_trove_dashboard
 
     elif [[ "$1" == "stack" && "$2" == "extra"  ]]; then
         # no-op
