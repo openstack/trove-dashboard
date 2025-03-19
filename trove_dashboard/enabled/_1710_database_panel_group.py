bb3713640f909ed76c3751d1cffbe76733514ff5
Ivan Anfimov <lazekteam@gmail.com>
2025-03-19 18:00:31 +0000

    Database renamed to Databases
    There can be an unlimited number of databases, this name would be more logical.
    
    Change-Id: Icea880e7cd9d238f4ae0831a7645fda6df8ed124

--- trove_dashboard/enabled/_1710_database_panel_group.py
+++ trove_dashboard/enabled/_1710_database_panel_group.py
@@ -15,6 +15,6 @@ from django.utils.translation import gettext_lazy as _
 # The slug of the panel group to be added to HORIZON_CONFIG. Required.
 PANEL_GROUP = 'database'
 # The display name of the PANEL_GROUP. Required.
-PANEL_GROUP_NAME = _('Database')
+PANEL_GROUP_NAME = _('Databases')
 # The slug of the dashboard the PANEL_GROUP associated with. Required.
 PANEL_GROUP_DASHBOARD = 'project'
